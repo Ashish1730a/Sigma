5ed002252d117c26e023373d3a39af63019d0206
Ashish1730a <singhashish1730@gmail.com>
2025-11-16 14:00:25 +0530

    basic lottery game

--- react-game/src/App.jsx
+++ react-game/src/App.jsx
@@ -4,12 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { LudoBoard } from './LudoBoard'
 import TodoList from './TodoList'
+import Lottery from './Lottery'
 
 function App() {
 
   return (
     <>
-      <TodoList />
+      <Lottery />
     </>
   )
 }
